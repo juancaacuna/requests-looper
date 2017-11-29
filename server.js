9dbb8831ab83188da12dd0354bc0c2f93b24eaac
JuanCa Acuna <jcarlos323@hotmail.com>
2017-11-29 09:23:26 -0600

    Add time interval

--- server.js
+++ server.js
@@ -10,19 +10,29 @@ const app = express()
 app.use(cors())
 
 let requestLoop = null
+const startHour = parseInt(process.env.START_UTC_TIME_HOUR)
+let endHour = parseInt(process.env.END_UTC_TIME_HOUR)
+if (startHour > endHour) {
+    endHour += 24
+}
 
 app.get('/', (req, res) => {
     if (requestLoop) {
         clearInterval(requestLoop)
     }
-    requestLoop = setInterval(() => {
-        data.Applications.forEach(url => {
-            const protocol = url.indexOf('https') >= 0 ? https : http
-            const req = protocol.get(url, function(res) {
-                console.log(`STATUS: ${res.statusCode} - ${url}`)
-            })
-        })
-    }, 60000)
+    if (data.Applications && data.Applications.length > 0) {
+        requestLoop = setInterval(() => {
+            const currentHour = new Date().getHours()
+            if (currentHour >= startHour && currentHour < endHour) {
+                data.Applications.forEach(url => {
+                    const protocol = url.indexOf('https') >= 0 ? https : http
+                    const req = protocol.get(url, function(res) {
+                        console.log(`STATUS: ${res.statusCode} - ${url}`)
+                    })
+                })
+            }
+        }, 60000)
+    }
     res.send('Requests in loop')
 })
 
